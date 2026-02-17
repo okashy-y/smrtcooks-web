1669071da6c6bb96a79f5931068164edbe445c8b
okashy-y <96489@kes.edu.kw>
2026-02-17 13:47:43 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const repo = "smrtcooks-web";
+
 const nextConfig = {
   output: "export",
-  images: { unoptimized: true }
+  images: { unoptimized: true },
+  trailingSlash: true,
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`
 };
+
 export default nextConfig;
